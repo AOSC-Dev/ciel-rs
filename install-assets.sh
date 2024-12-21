c08f508fad339d985eee6f1c367fa650ad08336f
xtex <xtexchooser@duck.com>
2024-12-22 00:11:40 +0800

    fix completions installation

--- install-assets.sh
+++ install-assets.sh
@@ -8,8 +8,8 @@ install -Dvm755 plugins/* "${PREFIX}/libexec/ciel-plugin"
 
 # install completions
 install -dv "${PREFIX}/share/zsh/functions/Completion/Linux/"
-install -Dvm644 completions/_ciel "${PREFIX}/share/zsh/functions/Completion/Linux/"
+install -Dvm644 cli/completions/_ciel "${PREFIX}/share/zsh/functions/Completion/Linux/"
 install -dv "${PREFIX}/share/fish/vendor_completions.d/"
-install -Dvm644 completions/ciel.fish "${PREFIX}/share/fish/vendor_completions.d/"
+install -Dvm644 cli/completions/ciel.fish "${PREFIX}/share/fish/vendor_completions.d/"
 install -dv "${PREFIX}/share/bash-completion/completions/"
-install -Dvm644 completions/ciel.bash "${PREFIX}/share/bash-completion/completions/"
+install -Dvm644 cli/completions/ciel.bash "${PREFIX}/share/bash-completion/completions/"
