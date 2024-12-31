AOSC-Dev/ciel-rs

commit 15a57f9271f434eb158c7743134dc284cd52750c

instance: replace rw-tree to ro-tree option

src/instance.rs
@@ -143,9 +143,9 @@ pub struct InstanceConfig {
     /// Set to `None` to disable tmpfs for filesystem.
     #[serde(default)]
     pub tmpfs: Option<TmpfsConfig>,
-    /// Whether TREE should be mounted as read-write.
+    /// Whether TREE should be mounted as read-only.
     #[serde(default)]
-    pub writable_tree: bool,
+    pub readonly_tree: bool,
 }
 
 impl Default for InstanceConfig {
@@ -156,7 +156,7 @@ impl Default for InstanceConfig {
             extra_nspawn_options: vec![],
             use_local_repo: true,
             tmpfs: None,
-            writable_tree: false,
+            readonly_tree: false,
         }
     }
 }

cli/completions/_ciel
@@ -94,7 +94,7 @@ _arguments "${_arguments_options[@]}" : \
 '--local-repo=[Enable local package repository]: :(true false)' \
 '--tmpfs=[Enable tmpfs]: :(true false)' \
 '--tmpfs-size=[Size of tmpfs to use, in MiB]: :_default' \
-'--rw-tree=[Mount TREE as read-write]: :(true false)' \
+'--ro-tree=[Mount TREE as read-only]: :(true false)' \
 '--add-repo=[Add an extra APT repository]:repo:_default' \
 '--remove-repo=[Remove an extra APT repository]:repo:_default' \
 '--add-nspawn-opt=[Add an extra nspawn option]:nspawn-opt:_default' \
@@ -113,7 +113,7 @@ _arguments "${_arguments_options[@]}" : \
 '--local-repo=[Enable local package repository]: :(true false)' \
 '--tmpfs=[Enable tmpfs]: :(true false)' \
 '--tmpfs-size=[Size of tmpfs to use, in MiB]: :_default' \
-'--rw-tree=[Mount TREE as read-write]: :(true false)' \
+'--ro-tree=[Mount TREE as read-only]: :(true false)' \
 '--add-repo=[Add an extra APT repository]:repo:_default' \
 '--remove-repo=[Remove an extra APT repository]:repo:_default' \
 '--add-nspawn-opt=[Add an extra nspawn option]:nspawn-opt:_default' \
@@ -159,7 +159,7 @@ _arguments "${_arguments_options[@]}" : \
 '--local-repo=[Enable local package repository]: :(true false)' \
 '--tmpfs=[Enable tmpfs]: :(true false)' \
 '--tmpfs-size=[Size of tmpfs to use, in MiB]: :_default' \
-'--rw-tree=[Mount TREE as read-write]: :(true false)' \
+'--ro-tree=[Mount TREE as read-only]: :(true false)' \
 '--add-repo=[Add an extra APT repository]:repo:_default' \
 '--remove-repo=[Remove an extra APT repository]:repo:_default' \
 '--add-nspawn-opt=[Add an extra nspawn option]:nspawn-opt:_default' \
@@ -239,7 +239,7 @@ _arguments "${_arguments_options[@]}" : \
 '(-i)--local-repo=[Enable local package repository]: :(true false)' \
 '(-i)--tmpfs=[Enable tmpfs]: :(true false)' \
 '(-i)--tmpfs-size=[Size of tmpfs to use, in MiB]: :_default' \
-'(-i)--rw-tree=[Mount TREE as read-write]: :(true false)' \
+'(-i)--ro-tree=[Mount TREE as read-only]: :(true false)' \
 '(-i)--add-repo=[Add an extra APT repository]:repo:_default' \
 '(-i)--remove-repo=[Remove an extra APT repository]:repo:_default' \
 '(-i)--add-nspawn-opt=[Add an extra nspawn option]:nspawn-opt:_default' \
@@ -266,7 +266,7 @@ _arguments "${_arguments_options[@]}" : \
 '(-i)--local-repo=[Enable local package repository]: :(true false)' \
 '(-i)--tmpfs=[Enable tmpfs]: :(true false)' \
 '(-i)--tmpfs-size=[Size of tmpfs to use, in MiB]: :_default' \
-'(-i)--rw-tree=[Mount TREE as read-write]: :(true false)' \
+'(-i)--ro-tree=[Mount TREE as read-only]: :(true false)' \
 '(-i)--add-repo=[Add an extra APT repository]:repo:_default' \
 '(-i)--remove-repo=[Remove an extra APT repository]:repo:_default' \
 '(-i)--add-nspawn-opt=[Add an extra nspawn option]:nspawn-opt:_default' \
@@ -278,6 +278,7 @@ _arguments "${_arguments_options[@]}" : \
 '(-i)--unset-nspawn-opt[Remove all extra nspawn option]' \
 '-g[Fetch package sources only]' \
 '--stage-select[Select the starting point for a build]' \
+'(-i)--always-discard[Destory ephemeral containers if the build fails]' \
 '-h[Print help]' \
 '--help[Print help]' \
 '*::PACKAGES:_default' \

cli/completions/ciel.bash
@@ -196,7 +196,7 @@ _ciel() {
             return 0
             ;;
         ciel__add)
-            opts="-h --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --rw-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --help <INSTANCE>"
+            opts="-h --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --ro-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --help <INSTANCE>"
             if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
                 COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                 return 0
@@ -214,7 +214,7 @@ _ciel() {
                     COMPREPLY=($(compgen -f "${cur}"))
                     return 0
                     ;;
-                --rw-tree)
+                --ro-tree)
                     COMPREPLY=($(compgen -W "true false" -- "${cur}"))
                     return 0
                     ;;
@@ -256,7 +256,7 @@ _ciel() {
             return 0
             ;;
         ciel__build)
-            opts="-i -g -c -h --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --rw-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --resume --stage-select --help [PACKAGES]..."
+            opts="-i -g -c -h --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --ro-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --resume --stage-select --always-discard --help [PACKAGES]..."
             if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
                 COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                 return 0
@@ -278,7 +278,7 @@ _ciel() {
                     COMPREPLY=($(compgen -f "${cur}"))
                     return 0
                     ;;
-                --rw-tree)
+                --ro-tree)
                     COMPREPLY=($(compgen -W "true false" -- "${cur}"))
                     return 0
                     ;;
@@ -824,7 +824,7 @@ _ciel() {
             return 0
             ;;
         ciel__instconf)
-            opts="-i -h --force-no-rollback --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --rw-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --help"
+            opts="-i -h --force-no-rollback --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --ro-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --help"
             if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
                 COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                 return 0
@@ -846,7 +846,7 @@ _ciel() {
                     COMPREPLY=($(compgen -f "${cur}"))
                     return 0
                     ;;
-                --rw-tree)
+                --ro-tree)
                     COMPREPLY=($(compgen -W "true false" -- "${cur}"))
                     return 0
                     ;;
@@ -1126,7 +1126,7 @@ _ciel() {
             return 0
             ;;
         ciel__shell)
-            opts="-i -h --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --rw-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --help [COMMANDS]..."
+            opts="-i -h --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --ro-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --help [COMMANDS]..."
             if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
                 COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                 return 0
@@ -1148,7 +1148,7 @@ _ciel() {
                     COMPREPLY=($(compgen -f "${cur}"))
                     return 0
                     ;;
-                --rw-tree)
+                --ro-tree)
                     COMPREPLY=($(compgen -W "true false" -- "${cur}"))
                     return 0
                     ;;
@@ -1190,7 +1190,7 @@ _ciel() {
             return 0
             ;;
         ciel__update__os)
-            opts="-h --force-use-apt --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --rw-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --help"
+            opts="-h --force-use-apt --local-repo --tmpfs --tmpfs-size --unset-tmpfs-size --ro-tree --add-repo --remove-repo --unset-repo --add-nspawn-opt --remove-nspawn-opt --unset-nspawn-opt --help"
             if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
                 COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                 return 0
@@ -1208,7 +1208,7 @@ _ciel() {
                     COMPREPLY=($(compgen -f "${cur}"))
                     return 0
                     ;;
-                --rw-tree)
+                --ro-tree)
                     COMPREPLY=($(compgen -W "true false" -- "${cur}"))
                     return 0
                     ;;

cli/completions/ciel.fish
@@ -83,7 +83,7 @@ complete -c ciel -n "__fish_ciel_using_subcommand load-os" -s h -l help -d 'Prin
 complete -c ciel -n "__fish_ciel_using_subcommand update-os" -l local-repo -d 'Enable local package repository' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand update-os" -l tmpfs -d 'Enable tmpfs' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand update-os" -l tmpfs-size -d 'Size of tmpfs to use, in MiB' -r
-complete -c ciel -n "__fish_ciel_using_subcommand update-os" -l rw-tree -d 'Mount TREE as read-write' -r -f -a "{true\t'',false\t''}"
+complete -c ciel -n "__fish_ciel_using_subcommand update-os" -l ro-tree -d 'Mount TREE as read-only' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand update-os" -l add-repo -d 'Add an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand update-os" -l remove-repo -d 'Remove an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand update-os" -l add-nspawn-opt -d 'Add an extra nspawn option' -r
@@ -97,7 +97,7 @@ complete -c ciel -n "__fish_ciel_using_subcommand instconf" -s i -d 'Instance to
 complete -c ciel -n "__fish_ciel_using_subcommand instconf" -l local-repo -d 'Enable local package repository' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand instconf" -l tmpfs -d 'Enable tmpfs' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand instconf" -l tmpfs-size -d 'Size of tmpfs to use, in MiB' -r
-complete -c ciel -n "__fish_ciel_using_subcommand instconf" -l rw-tree -d 'Mount TREE as read-write' -r -f -a "{true\t'',false\t''}"
+complete -c ciel -n "__fish_ciel_using_subcommand instconf" -l ro-tree -d 'Mount TREE as read-only' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand instconf" -l add-repo -d 'Add an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand instconf" -l remove-repo -d 'Remove an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand instconf" -l add-nspawn-opt -d 'Add an extra nspawn option' -r
@@ -126,7 +126,7 @@ complete -c ciel -n "__fish_ciel_using_subcommand load-tree" -s h -l help -d 'Pr
 complete -c ciel -n "__fish_ciel_using_subcommand add" -l local-repo -d 'Enable local package repository' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand add" -l tmpfs -d 'Enable tmpfs' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand add" -l tmpfs-size -d 'Size of tmpfs to use, in MiB' -r
-complete -c ciel -n "__fish_ciel_using_subcommand add" -l rw-tree -d 'Mount TREE as read-write' -r -f -a "{true\t'',false\t''}"
+complete -c ciel -n "__fish_ciel_using_subcommand add" -l ro-tree -d 'Mount TREE as read-only' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand add" -l add-repo -d 'Add an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand add" -l remove-repo -d 'Remove an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand add" -l add-nspawn-opt -d 'Add an extra nspawn option' -r
@@ -152,7 +152,7 @@ complete -c ciel -n "__fish_ciel_using_subcommand shell" -s i -d 'Instance to be
 complete -c ciel -n "__fish_ciel_using_subcommand shell" -l local-repo -d 'Enable local package repository' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand shell" -l tmpfs -d 'Enable tmpfs' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand shell" -l tmpfs-size -d 'Size of tmpfs to use, in MiB' -r
-complete -c ciel -n "__fish_ciel_using_subcommand shell" -l rw-tree -d 'Mount TREE as read-write' -r -f -a "{true\t'',false\t''}"
+complete -c ciel -n "__fish_ciel_using_subcommand shell" -l ro-tree -d 'Mount TREE as read-only' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand shell" -l add-repo -d 'Add an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand shell" -l remove-repo -d 'Remove an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand shell" -l add-nspawn-opt -d 'Add an extra nspawn option' -r
@@ -167,7 +167,7 @@ complete -c ciel -n "__fish_ciel_using_subcommand build" -s i -d 'Instance to be
 complete -c ciel -n "__fish_ciel_using_subcommand build" -l local-repo -d 'Enable local package repository' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand build" -l tmpfs -d 'Enable tmpfs' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand build" -l tmpfs-size -d 'Size of tmpfs to use, in MiB' -r
-complete -c ciel -n "__fish_ciel_using_subcommand build" -l rw-tree -d 'Mount TREE as read-write' -r -f -a "{true\t'',false\t''}"
+complete -c ciel -n "__fish_ciel_using_subcommand build" -l ro-tree -d 'Mount TREE as read-only' -r -f -a "{true\t'',false\t''}"
 complete -c ciel -n "__fish_ciel_using_subcommand build" -l add-repo -d 'Add an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand build" -l remove-repo -d 'Remove an extra APT repository' -r
 complete -c ciel -n "__fish_ciel_using_subcommand build" -l add-nspawn-opt -d 'Add an extra nspawn option' -r
@@ -178,6 +178,7 @@ complete -c ciel -n "__fish_ciel_using_subcommand build" -l unset-repo -d 'Remov
 complete -c ciel -n "__fish_ciel_using_subcommand build" -l unset-nspawn-opt -d 'Remove all extra nspawn option'
 complete -c ciel -n "__fish_ciel_using_subcommand build" -s g -d 'Fetch package sources only'
 complete -c ciel -n "__fish_ciel_using_subcommand build" -l stage-select -d 'Select the starting point for a build'
+complete -c ciel -n "__fish_ciel_using_subcommand build" -l always-discard -d 'Destory ephemeral containers if the build fails'
 complete -c ciel -n "__fish_ciel_using_subcommand build" -s h -l help -d 'Print help'
 complete -c ciel -n "__fish_ciel_using_subcommand repo; and not __fish_seen_subcommand_from refresh help" -s h -l help -d 'Print help'
 complete -c ciel -n "__fish_ciel_using_subcommand repo; and not __fish_seen_subcommand_from refresh help" -f -a "refresh" -d 'Refresh the repository'

cli/src/cli.rs
@@ -116,9 +116,9 @@ pub fn build_cli() -> Command {
             .help("Reset tmpfs size to default")
             .action(ArgAction::SetTrue),
         // read-write tree
-        Arg::new("rw-tree")
-            .long("rw-tree")
-            .help("Mount TREE as read-write")
+        Arg::new("ro-tree")
+            .long("ro-tree")
+            .help("Mount TREE as read-only")
             .value_parser(value_parser!(bool)),
     ];
     instance_configs.extend(config_list(

cli/src/config.rs
@@ -126,7 +126,7 @@ pub fn patch_instance_config(args: &ArgMatches, config: &mut InstanceConfig) -> 
     config_list(args, "repo", &mut config.extra_apt_repos);
     config_list(args, "nspawn-opt", &mut config.extra_nspawn_options);
     config_bool(args, "local-repo", &mut config.use_local_repo);
-    config_bool(args, "rw-tree", &mut config.writable_tree);
+    config_bool(args, "ro-tree", &mut config.readonly_tree);
 
     Ok(())
 }

src/container.rs
@@ -507,7 +507,7 @@ fn setup_machine(container: &Container) -> Result<()> {
         container.ns_name
     );
 
-    machine.bind(workspace_dir.join("TREE"), "/tree".into(), !instance_config.writable_tree)?;
+    machine.bind(workspace_dir.join("TREE"), "/tree".into(), instance_config.readonly_tree)?;
     if !workspace_config.no_cache_packages {
         machine.bind(
             workspace_dir.join("CACHE"),

commit 07cc30a06b72287b6793ef24f52e03c6292c9d24

instance: fix test_instance_config for ro-tree update
Fixes: 15a57f9271f4 ("instance: replace rw-tree to ro-tree option")

src/instance.rs
@@ -247,7 +247,7 @@ mod test {
 extra-apt-repos = []
 extra-nspawn-options = []
 use-local-repo = true
-writable-tree = false
+readonly-tree = false
 "##
         );
         assert_eq!(InstanceConfig::parse(&serialized).unwrap(), config);