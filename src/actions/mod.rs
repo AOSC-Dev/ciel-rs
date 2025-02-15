642799d47ce96d1eecae9378182f5c1a1e42cc5f
eatradish <sakiiily@aosc.io>
2025-02-15 18:03:45 +0800

    fix: do not clean packages cache when use `oma` to update-os

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -19,7 +19,7 @@ const DEFAULT_MOUNTS: &[(&str, &str)] = &[
     ("CACHE", "/var/cache/apt/archives"),
 ];
 const APT_UPDATE_SCRIPT: &str = r#"export DEBIAN_FRONTEND=noninteractive;apt-get update -y --allow-releaseinfo-change && apt-get -y -o Dpkg::Options::="--force-confnew" full-upgrade --autoremove --purge && apt autoclean"#;
-const OMA_UPDATE_SCRIPT: &str = r#"oma upgrade -y --force-confnew --no-progress --force-unsafe-io && oma autoremove -y --no-progress --remove-config && oma clean --no-progress"#;
+const OMA_UPDATE_SCRIPT: &str = r#"oma upgrade -y --force-confnew --no-progress --force-unsafe-io && oma autoremove -y --no-progress --remove-config"#;
 
 type MountOptions = (Vec<String>, Vec<(String, &'static str)>);
 /// Ensure that the directories exist and mounted
